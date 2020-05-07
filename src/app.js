e29dca1c24c340eb9e3c2154121d571b6d37a27f
Joseph Montesano <jmmmontesano@gmail.com>
2020-05-07 17:44:25 -0400

    added production error for debug

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.get('/xss', (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
+    response = { error: { message: error.message, error } };
   } else {
     console.error(error);
     response = { message: error.message, error };

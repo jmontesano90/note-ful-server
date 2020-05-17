cbe30906ddf51ca13d383baee2dbfb82751e65d0
Joseph Montesano <jmmmontesano@gmail.com>
2020-05-17 15:43:24 -0400

    THE TESTS CONTINUE

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -24,8 +24,8 @@ notesRouter
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
-    const { name, folderid, content, modified } = req.body;
-    const newNote = { name, folderid, content, modified };
+    const { name, folderid, content } = req.body;
+    const newNote = { name, folderid, content };
 
     for (const [key, value] of Object.entries(newNote)) {
       if (value == null) {
